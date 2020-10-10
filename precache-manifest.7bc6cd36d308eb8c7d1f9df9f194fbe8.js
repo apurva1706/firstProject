d6debe4f3b59b5c4b490c1be1a38215c7ee603e4
apurva1706 <apurva.peri2001@gmail.com>
2020-10-10 13:35:03 +0530

    first commit

---
+++ precache-manifest.7bc6cd36d308eb8c7d1f9df9f194fbe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ec63776477e9ee6e76da921f9ece26",
+    "url": "/firstProject/index.html"
+  },
+  {
+    "revision": "7dbc68717c5e295acea1",
+    "url": "/firstProject/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "92d74974c0de4a22a229",
+    "url": "/firstProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dbc68717c5e295acea1",
+    "url": "/firstProject/static/js/2.c84a817c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstProject/static/js/2.c84a817c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d74974c0de4a22a229",
+    "url": "/firstProject/static/js/main.c8bead46.chunk.js"
+  },
+  {
+    "revision": "4889b3905c4970e79c09",
+    "url": "/firstProject/static/js/runtime-main.261eef3d.js"
+  }
+]);
